bb174f7d128d4989405b3a0be0c088aa90aaff76
RyuKai3791 <pabloluque2000@gmail.com>
2024-02-08 22:49:09 +0100

    Actualizacion 08/02/2024

---
+++ Gallery/js/preloader.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function() {
+    // ESTABLECER UN TIEMPO MÍNIMO DE CARGA
+    let tiempoMinimo = 2600;
+
+    // OBTENER EL TIEMPO ACTUAL
+    let tiempoInicio = new Date().getTime();
+
+    let tiempoRestante = tiempoMinimo - (new Date().getTime() - tiempoInicio);
+
+    if (tiempoRestante > 0) {
+        setTimeout(function() {
+            document.getElementById('preloader').style.display = 'none';
+            document.getElementById('contenido').style.display = 'block';
+        }, tiempoRestante);
+
+    } else {
+        // SI YA HA PASADO EL TIEMPO MÍNIMO, OCULTA EL PRELOADER INMEDIATAMENTE
+        document.getElementById('preloader').style.display = 'none';
+        document.getElementById('contenido').style.display = 'block';
+        var bodyElement = document.body;
+        bodyElement.style.animation = '';
+    }
+});
